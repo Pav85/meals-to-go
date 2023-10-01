e21e43aa89bbc8d6494dc3cbba3eb87b09bbb957
Pav85 <pawel.werbowy@gmail.com>
2023-10-01 11:59:05 +0100

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function App() {
   return (
-    <View>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Hey this is just a blank canvas</Text>
     </View>
   );
