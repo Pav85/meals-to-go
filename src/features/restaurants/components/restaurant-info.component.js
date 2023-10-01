212355e4819b815ec338b572c00b93d7eafea5f8
Pav85 <pawel.werbowy@gmail.com>
2023-10-01 20:31:02 +0100

    Update restaurant-info.component.js

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -2,5 +2,14 @@ import React from "react";
 import { Text } from "react-native";
 
 export const RestaurantInfo = ({ restaurant }) => {
+  const {
+    name,
+    icon,
+    photos,
+    address,
+    openingHours,
+    rating,
+    isClosedTemporarily,
+  } = restaurant;
   return <Text>Info</Text>;
 };
