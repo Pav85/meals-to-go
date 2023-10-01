b1eb747714233dc0f7f29049abbda003a6cba427
Pav85 <pawel.werbowy@gmail.com>
2023-10-01 11:56:59 +0100

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,11 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 export default function App() {
-  return <View></View>;
+  return (
+    <View>
+      <Text>Hey this is just a blank canvas</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
