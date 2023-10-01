06ded3fc01005259de2c8eea33fa4f55e9e2a43a
Pav85 <pawel.werbowy@gmail.com>
2023-10-01 12:45:46 +0100

    Update App.js

--- App.js
+++ App.js
@@ -4,17 +4,14 @@ import React from "react";
 
 export default function App() {
   return (
-    <>
-      <SafeAreaView style={{ flex: 1 }}>
-        <View style={{ padding: 16, backgroundColor: "green" }}>
-          <Text>Search</Text>
-        </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
-      <StatusBar style="auto" />
-    </>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ padding: 16, backgroundColor: "green" }}>
+        <Text>Search</Text>
+      </View>
+      <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+        <Text>List</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
