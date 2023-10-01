10d508ab374279833e5bb008a894f5095f0f8675
Pav85 <pawel.werbowy@gmail.com>
2023-10-01 20:38:56 +0100

    Update restaurant-info.component.js

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -9,7 +9,7 @@ export const RestaurantInfo = ({ restaurant }) => {
       "https://www.allrecipes.com/thmb/fFW1o307WSqFFYQ3-QXYVpnFj6E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/48727-Mikes-homemade-pizza-DDMFS-beauty-4x3-BG-2974-a7a9842c14e34ca699f3b7d7143256cf.jpg",
     ],
     address = "100 random street",
-    openingHours,
+    isOpenNow = true,
     rating,
     isClosedTemporarily,
   } = restaurant;
