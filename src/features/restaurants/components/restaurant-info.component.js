5c93ba6ff427eb884b500ac4d134139eb211cff0
Pav85 <pawel.werbowy@gmail.com>
2023-10-02 15:42:43 +0100

    styled components

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
+import styled from "styled-components";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
