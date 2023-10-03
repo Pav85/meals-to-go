d11ed31873b940a6d8c1bc803ffd566f2f49e16c
Pav85 <pawel.werbowy@gmail.com>
2023-10-03 07:19:16 +0100

    Update index.js

--- src/infrastructure/theme/index.js
+++ src/infrastructure/theme/index.js
@@ -2,3 +2,13 @@ import { colors } from "./colors";
 import { sizes } from "./sizes";
 import { space, lineHeights } from "./spacing";
 import { fonts, fontSizes, fontWeights } from "./fonts";
+
+export const theme = {
+  colors,
+  sizes,
+  space,
+  lineHeights,
+  fonts,
+  fontSizes,
+  fontWeights,
+};
