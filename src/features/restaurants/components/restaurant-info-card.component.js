40fbe32aaaf95638f1b32fff884f32a9ec1341b0
Pav85 <pawel.werbowy@gmail.com>
2023-10-04 13:00:05 +0100

    Update restaurant-info-card.component.js

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -47,6 +49,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       ></RestaurantCardCover>
       <Info>
         <Title>{name}</Title>
+        <SvgXml xml={star} />
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
