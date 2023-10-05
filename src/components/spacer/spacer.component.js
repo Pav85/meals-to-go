c6cb693abf63c26ae7cd553bc213944e1c7dde28
Pav85 <pawel.werbowy@gmail.com>
2023-10-05 10:11:55 +0100

    Update spacer.component.js

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,4 +1,9 @@
 import React from "react";
 import { View } from "react-native";
+import styled from "styled-components";
 
-export const Spacer = { variant }=>{};
+export const Spacer = { variant }=>{
+    switch(variant) {
+        default:
+    }
+};
