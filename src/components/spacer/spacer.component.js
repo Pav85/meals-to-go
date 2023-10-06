67a42780484224a946a805a28f8da026ffbc9a68
Pav85 <pawel.werbowy@gmail.com>
2023-10-06 11:56:34 +0100

    Update spacer.component.js

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,28 +1,4 @@
 import React from "react";
 import styled from "styled-components/native";
 
-const TopSmall = styled.View`
-  margin-top: ${(props) => props.theme.space[1]};
-`;
-
-const TopMedium = styled.View`
-  margin-top: ${(props) => props.theme.space[2]};
-`;
-
-const TopLarge = styled.View`
-  margin-top: ${(props) => props.theme.space[3]};
-`;
-
-const LeftSmall = styled.View`
-  margin-left: ${(props) => props.theme.space[1]};
-`;
-
-const LeftMedium = styled.View`
-  margin-left: ${(props) => props.theme.space[2]};
-`;
-
-const LeftLarge = styled.View`
-  margin-left: ${(props) => props.theme.space[3]};
-`;
-
 export const Spacer = styled.View``;
