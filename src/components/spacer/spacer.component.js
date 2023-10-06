3ef49bcf97785f30d5d1075012e74dc3e781229e
Pav85 <pawel.werbowy@gmail.com>
2023-10-06 12:00:38 +0100

    Update spacer.component.js

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,6 +1,12 @@
 import React from "react";
 import styled from "styled-components/native";
 
+const sizes = {
+  small: 1,
+  medium: 2,
+  large: 3,
+};
+
 export const Spacer = styled.View`
   ${({ position, size }) => {}}
 `;
