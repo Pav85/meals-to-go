689ed4286e42fa03d32eee9df5407e95f29084f4
Pav85 <pawel.werbowy@gmail.com>
2023-10-06 11:59:41 +0100

    Update spacer.component.js

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,4 +1,11 @@
 import React from "react";
 import styled from "styled-components/native";
 
-export const Spacer = styled.View``;
+export const Spacer = styled.View`
+  ${({ position, size }) => {}}
+`;
+
+Spacer.defaultProps = {
+  position: "top",
+  size: "small",
+};
