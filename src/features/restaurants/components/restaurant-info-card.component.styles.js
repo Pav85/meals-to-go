4234eeacaa8cdb7aae5993c1080e3923f03c4437
Pav85 <pawel.werbowy@gmail.com>
2023-10-08 11:36:14 +0100

    Update restaurant-info-card.component.styles.js

--- src/features/restaurants/components/restaurant-info-card.component.styles.js
+++ src/features/restaurants/components/restaurant-info-card.component.styles.js
@@ -1,1 +1,41 @@
 import styled from "styled-components/native";
+
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+const Icon = styled.Image`
+  width: 15px;
+  height: 15px;
+`;
