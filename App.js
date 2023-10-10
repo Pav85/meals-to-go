9532083c335f0563f812519e837b7cb05e192e7e
Pav85 <pawel.werbowy@gmail.com>
2023-10-10 09:58:02 +0100

    Update App.js

--- App.js
+++ App.js
@@ -15,17 +15,16 @@ import { SafeArea } from "./src/components/utility/safe-area.component";
 
 const Tab = createBottomTabNavigator();
 
-const Settings = () => {
+const Settings = () => (
   <SafeArea>
     <Text>Settings</Text>
-  </SafeArea>;
-};
-
-const Map = () => {
+  </SafeArea>
+);
+const Map = () => (
   <SafeArea>
     <Text>Map</Text>
-  </SafeArea>;
-};
+  </SafeArea>
+);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
