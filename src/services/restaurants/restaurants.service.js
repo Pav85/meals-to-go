0a3d1a0ab425f20990cd1d3c1b6401d31f6ddf51
Pav85 <pawel.werbowy@gmail.com>
2023-10-11 21:14:37 +0100

    service update

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,7 @@
-import  { mocks } './mock'
-const restaurantRequest = (location = "37.7749295,-122.4194155") => {
-    console.log(mocks)
+import { mocks } from "./mock";
+
+export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
+  console.log(mocks);
 };
+
+restaurantsRequest();
