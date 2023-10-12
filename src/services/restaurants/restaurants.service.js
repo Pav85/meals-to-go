74a87582da6272830680562d28b3670c193152e6
Pav85 <pawel.werbowy@gmail.com>
2023-10-12 09:20:45 +0100

    camelize

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,19 +1,25 @@
 import { mocks } from "./mock";
+import camelize from "camelize";
 
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
     if (!mock) {
-      reject("no location found");
+      reject("not found");
     }
     resolve(mock);
   });
 };
 
+const restaurantsTransform = (result) => {
+  const newResult = camelize(result);
+  return newResult;
+};
 restaurantsRequest()
-  .then((result) => {
-    console.log(result);
+  .then(restaurantsTransform)
+  .then((transformedResponse) => {
+    console.log(transformedResponse);
   })
   .catch((err) => {
-    console.log("error: " + err);
+    console.log("error");
   });
