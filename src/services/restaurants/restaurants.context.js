4f0cf9de47d6a26b138369160791cbc16338b4a2
Pav85 <pawel.werbowy@gmail.com>
2023-10-15 20:38:42 +0100

    Update restaurants.context.js

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -7,6 +7,10 @@ import {
 export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = ({ children }) => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   return (
     <RestaurantsContext.Provider
       value={{ restaurants: [1, 2, 3, 4, 5, 6, 7, 8] }}
