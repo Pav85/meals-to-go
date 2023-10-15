2db929934bf7e4d27af6655b05025f47369b63dc
Pav85 <pawel.werbowy@gmail.com>
2023-10-15 20:31:56 +0100

    Update restaurants.context.js

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -8,7 +8,9 @@ export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = ({ children }) => {
   return (
-    <RestaurantsContext.Provider value={{ restaurants: [1, 2, 3] }}>
+    <RestaurantsContext.Provider
+      value={{ restaurants: [1, 2, 3, 4, 5, 6, 7, 8] }}
+    >
       {children}
     </RestaurantsContext.Provider>
   );
