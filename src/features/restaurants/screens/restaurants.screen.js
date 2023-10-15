1144da4e4fd5f5c1a00a47083ea4ce52b4c01799
Pav85 <pawel.werbowy@gmail.com>
2023-10-15 20:27:52 +0100

    Update restaurants.screen.js

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -12,6 +12,7 @@ import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import styled from "styled-components/native";
 import { SafeArea } from "../../../components/utility/safe-area.component";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
@@ -29,6 +30,7 @@ const RestaurantList = styled(FlatList).attrs({
 })``;
 
 export const RestaurantScreen = () => {
+  const restaurantContext = useContext(RestaurantsContext);
   return (
     <SafeArea>
       <SearchContainer>
