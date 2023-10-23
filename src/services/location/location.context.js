75a1bc9d8aefdf0e013c607dc16482e6d910ffb3
Pawel <pawel.werbowy@gmail.com>
2023-10-23 12:11:25 +0100

    Update location.context.js

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -5,6 +5,10 @@ import { locationRequest, locationTransform } from "./location.service";
 export const LocationContext = React.createContext();
 
 export const LocationContextProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [keyword, setKeyword] = useState("");
   return (
     <LocationContext.Provider
       value={{ isLoading, error, location, search: () => null, keyword }}
