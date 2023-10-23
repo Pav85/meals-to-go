36f63be6cbeab3c520b623371c7824e6033d871d
Pawel <pawel.werbowy@gmail.com>
2023-10-23 12:08:14 +0100

    Update location.context.js

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -1,3 +1,9 @@
 import React, { useState } from "react";
 
 import { locationRequest, locationTransform } from "./location.service";
+
+export const LocationContext = React.createContext();
+
+export const LocationContextProvider = ({ children }) => {
+  return <LocationContext.Provider>{children}</LocationContext.Provider>;
+};
