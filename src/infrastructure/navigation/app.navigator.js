1a900e68f2c805e985988bea64437cdebecd9879
Pawel <pawel.werbowy@gmail.com>
2023-10-30 22:49:48 +0000

    Create app.navigator.js

---
+++ src/infrastructure/navigation/app.navigator.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { Text } from "react-native";
+
+import { RestaurantScreen } from "../../features/restaurants/screens/restaurants.screen";
+import { SafeArea } from "../../components/utility/safe-area.component";
+
+const Tab = createBottomTabNavigator();
